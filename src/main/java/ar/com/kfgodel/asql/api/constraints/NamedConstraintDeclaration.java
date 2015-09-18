56312fc930088bef15b76a82cc937765aa65b74b
Dario L Garcia <kfgodel@gmail.com>
2015-09-18 15:01:41 -0500

    Added agnosticConstruct para los pedazos de código

--- src/main/java/ar/com/kfgodel/asql/api/constraints/NamedConstraintDeclaration.java
+++ src/main/java/ar/com/kfgodel/asql/api/constraints/NamedConstraintDeclaration.java
@@ -1,12 +1,12 @@
 package ar.com.kfgodel.asql.api.constraints;
 
-import ar.com.kfgodel.asql.impl.lang.Parseable;
+import ar.com.kfgodel.asql.api.AgnosticConstruct;
 import ar.com.kfgodel.asql.impl.model.constraints.NamedConstraintDeclarationModel;
 
 /**
  * Created by kfgodel on 16/07/15.
  */
-public interface NamedConstraintDeclaration extends Parseable {
+public interface NamedConstraintDeclaration extends AgnosticConstruct {
 
     @Override
     NamedConstraintDeclarationModel parseModel();
